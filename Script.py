b2c64911d4cae1694ed690f55402c2f5e318c2d1
Tempgit70 <tempgit70@gmail.com>
2024-12-31 22:45:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,9 +13,9 @@ CLONE_START_TXT = """<b>Hᴇʟʟᴏ {},ᴍʏ ɴᴀᴍᴇ {},【ɪ ᴀᴍ ʟᴀ�
 
 ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ ᴛʜᴇɴ ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ ғʀᴏᴍ ᴍʏ <a href=https://t.me/vj_botz>ᴘᴀʀᴇɴᴛ</a></b>"""
 
-    ABOUT_TXT = """<b><blockquote>I am Parmanent File Store Bot you can share your videos or files and get shareable link</blockquote>
+    ABOUT_TXT = """<b><blockquote><i>I am Parmanent File Store Bot you can share your videos or files and get shareable link</i></blockquote></b>
 
-🧑🏻‍💻 <i>Devloper:</i> <a href=https://t.me/Only_botsz>𝕆𝕟𝕝𝕪 𝔹𝕠𝕥𝕤𝕫</a></b>"""
+<b>🧑🏻‍💻 <i>Devloper:</i> <a href=https://t.me/Only_botsz>𝕆𝕟𝕝𝕪 𝔹𝕠𝕥𝕤𝕫</a></b>"""
 #👥 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ: <a href=https://t.me/VJ_Bot_Disscussion>𝐕𝐉 𝐒𝐮𝐩𝐩𝐨𝐫𝐭</a>
 
 #📢 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ: <a href=https://t.me/vj_botz>𝐕𝐉 𝐔𝐩𝐝𝐚𝐭𝐞</a></b>
